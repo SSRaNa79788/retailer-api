6cf6ba9add0100718cd6a0cc1b872b608f892245
SSRaNa79788 <84072393+SSRaNa79788@users.noreply.github.com>
2021-07-26 16:41:03 +0530

    Updated Routers/Router.go file

--- Routes/Routes.go
+++ Routes/Routes.go
@@ -1,1 +1,21 @@
 package Routes
+
+import(
+	"retailer-api/Controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine{
+	r:=gin.Default()
+	grp := r.Group("/")
+	{
+		grp.POST("product",Controllers.AddProduct)
+		grp.PATCH("product/:id",Controllers.UpdateProduct)
+		grp.GET("product/:id",Controllers.GetProduct)
+		grp.GET("products",Controllers.GetProducts)
+
+		grp.POST("order",Controllers.PlaceOrder)
+		grp.GET("order/:id",Controllers.GetOrder)
+	}
+	return r
+}
