cef26d30ec51367330cdfe50313c6602410954ba
SSRaNa79788 <84072393+SSRaNa79788@users.noreply.github.com>
2021-07-26 22:24:01 +0530

    updated 'ProductModel.go' file

--- Models/ProductModel.go
+++ Models/ProductModel.go
@@ -1,15 +1,35 @@
 package Models
 
-func AddProduct()(err error){
+import "retailer-api/Config"
 
+func AddProduct(order *Order)(err error){
+	if err:= Config.DB.Create(&order).Error ; err!=nil{
+		return err
+	}else{
+		return nil
+	}
 }
-func UpdateProduct()(err error){
 
+func UpdateProduct(order *Order)(err error){
+	if err := Config.DB.Save(&order).Error ; err!=nil{
+		return err
+	}else{
+		return nil
+	}
 }
-func GetProduct()(err error){
 
+func GetProduct(order *Order, product_id int)(err error){
+	if err := Config.DB.Where("id=?",product_id).First(order).Error ; err!=nil{
+		return err
+	}else{
+		return nil
+	}
 }
-func GetProducts()(err error){
-
+func GetProducts(order []*Order)(err error){
+	if err := Config.DB.Find(order).Error ; err!=nil{
+		return err
+	}else{
+		return nil
+	}
 }
 
