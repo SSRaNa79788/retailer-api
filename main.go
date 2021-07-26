9ef6646ba69980091d7eb86ded0e7c6f762ad3f9
SSRaNa79788 <84072393+SSRaNa79788@users.noreply.github.com>
2021-07-26 16:50:51 +0530

    Updated 'main.go' file

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
-package retailer_api
+package main
+
+import (
+	"fmt"
+	"retailer-api/Routes"
+	"retailer-api/Config"
+	"retailer-api/Models"
+	"github.com/jinzhu/gorm"
+)
+
+func main(){
+	//create database conection
+	Config.DB ,err = gorm.Open("mysql",Config.DbURL(Config.BuildDBConfig()))
+
+	//check for error in building database connection
+	if err!=nil{
+		fmt.Println("status: ",err)
+	}
+
+	//at last, Close the DB connection
+	defer Config.DB.Close()
+
+	//Do automigration
+	Config.DB.AutoMigrate(&Models.Order{},&Models.Product{})
+
+	//setup the routing confg
+	r:=Routes.SetupRouter()
+
+	//Run the router
+	r.Run()
+}
